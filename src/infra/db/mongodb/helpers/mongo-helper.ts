0019230512c63dd5af92e44d2d66bb00af42ed8f
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-03-01 13:04:05 -0300

    fix: bug typo

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -11,16 +11,14 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
-    this.client = null
   },
 
-  async getCollection (name: string): Promise<Collection> {
-    await this.connect(this.uri)
-    return this.client.db().collection(name)
+  getCollection (name: string): Collection {
+    return this.client.db().collection('name')
   },
 
   map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
-    return Object.assign({}, collectionWithoutId, { id: _id })
+    return { ...collectionWithoutId, id: _id }
   }
 }
