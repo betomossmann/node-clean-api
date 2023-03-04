b9d89c16d4ca4a8334f8a001b5ed5cb10dbc16f8
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-03-04 16:47:44 -0300

    refactor: move Validation to protocols folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,8 +1,8 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
-import { serverError, ok } from '../../presentation/helpers/http-helper'
 import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
+import { ok, serverError } from '../../presentation/helpers/http/http-helper'
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
--- src/presentation/helpers/validators/email-validation.ts
+++ src/presentation/helpers/validators/email-validation.ts
@@ -1,6 +1,6 @@
-import { Validation } from './validation'
 import { EmailValidator } from '../../protocols/presentation'
 import { InvalidParamError } from '../../errors'
+import { Validation } from '../../protocols/validation'
 
 export class EmailValidation implements Validation {
   private readonly fieldName: string
