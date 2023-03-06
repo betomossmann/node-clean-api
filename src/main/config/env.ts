fa11bcb63c48311a163a2a7a12598e2d4b2a315c
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-03-06 13:30:25 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/node-clean-api',
-  port: process.env.PORT || 6060
+  port: process.env.PORT || 6060,
+  jwtSecret: process.env.JWT_SECRET || 'tj654=*5BM'
 }
