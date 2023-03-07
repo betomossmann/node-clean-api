2894b6101a4ea3552228b89c7114f9a5735fca3f
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-03-07 07:59:55 -0300

    refactor: change models to use type alias instead of interfaces

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -38,7 +38,7 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
-interface SutTypes {
+type SutTypes = {
   sut: SignUpController
   addAccountStub: AddAccount
   validationStub: Validation
