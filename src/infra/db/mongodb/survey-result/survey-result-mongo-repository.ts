756c7dad04e75ac6d7d9a7ea662df9e3b9c80ae6
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-03-10 09:56:44 -0300

    test: ensure SurveyResultMongoRepository updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,4 +1,4 @@
-import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SaveSurveyResultRepository } from '@/data/protocols/survey/save-survey-result-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
@@ -15,8 +15,7 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
         date: data.date
       }
     }, {
-      upsert: true,
-      returnOriginal: false
+      upsert: true
     })
     return res.value && MongoHelper.map(res.value)
   }
