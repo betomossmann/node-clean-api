b824371e37671e4de90aa203125f997a7d72fd03
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-03-13 08:02:20 -0300

    docs: add login docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'API Documentation Node Clean',
     description: 'API de login e enquetes',
-    version: '1.0.0'
+    version: '1.0.0',
+    contact: {
+      name: 'Gilberto Mossmann',
+      url: 'https://www.linkedin.com/in/gilbertomossmann'
+    }
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
