44dc6364e1aff35602792db997cb35937f199e7b
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-03-21 16:59:09 -0300

    refactor: add sucrase and update files

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,6 +6,6 @@ MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const { setupApp } = await import('./config/app')
     const app = await setupApp()
-    app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
+    app.listen(env.port, () => { console.log(`Server running at http://0.0.0.0:${env.port}`) })
   })
   .catch(console.error)
